3beeae961e10e0d54a28e8dce2bc7b96cca39bba
Vianney Tran <viq.dev@gmail.com>
2014-10-13 20:08:14 +0200

    Implemented MurmurHash2 & fixed warnings (MurmurHash2 instead of mt19937)
    See here for MurmurHash2: https://sites.google.com/site/murmurhash/
    It is much more faster than mt19937 and still seems very random.

--- bluemars/perlin.cpp
+++ bluemars/perlin.cpp
@@ -4,26 +4,45 @@
 #include <stdlib.h>
 #include <perlin.h>
 
+long murmurHash2(long seed)
+{
+	const unsigned int m = 0x5bd1e995;
+	const int r = 24;
+	long h = seed;
+	long k = (unsigned int)(seed && 0xFF);
+	k *= m;
+	k ^= k >> r;
+	k *= m;
+	h *= m;
+	h ^= k;
+	h ^= (unsigned int)((seed >> 24) && 0xFF) << 16;
+	h ^= (unsigned int)((seed >> 16) && 0xFF) << 8;
+	h ^= (unsigned int)((seed >> 8) && 0xFF);
+	h *= m;
+	h ^= h >> 13;
+	h *= m;
+	h ^= h >> 15;
+	return h;
+}
+
 Noise::Noise(long s)
 {
     seed = s;
 }
 
-double Noise::discreteNoise(long x, long y, long octave)
+double Noise::discreteNoise(long x, long y, int octave)
 {
-    int amplitude = BASE_AMPLITUDE * pow(PERSISTENCE, octave);
+    int amplitude = (int) (BASE_AMPLITUDE * pow(PERSISTENCE, octave));
 	long temp_seed = (seed * 524288) % MAX_SEED;
 	temp_seed = (temp_seed + (15859 * x)) % MAX_SEED;
 	temp_seed = (temp_seed + (11117 * y)) % MAX_SEED;
 	temp_seed = (temp_seed + (28433 * octave)) % MAX_SEED;
-	rng.seed(temp_seed);
-	long random_value = rng();
+	long random_value = murmurHash2(temp_seed);
 	double ret = random_value % amplitude;
 	if (ret < 0) // Because % is not modulo !
 	{
 		ret += amplitude;
 	}
-	ret = ret + MEAN_NOISE_VALUE - amplitude / 2;//retourne un nombre aléatoire entre MEAN_NOISE_VALUE + amplitude/2 et MEAN_NOISE_VALUE - amplitude/2
 	return ret;
 }
 
@@ -43,11 +62,11 @@ double Noise::cosineInterpolate(long X1, long X2, double Z1, double Z2, long x) 
 
 double Noise::interpolatedNoise(long x, long y, long octave)
 {
-    double waveLength = 1.0f * BASE_WAVE_LENGTH / pow(2,octave);
+	long waveLength = static_cast<long>(1.0f * BASE_WAVE_LENGTH / pow(2, octave));
     //on détermine les quatre points les plus proches de (x,y) et entre lesquels il faut faire l'interpolation :(X1,Y1), (X1,Y2), (X2,Y1), (X2,Y2)
-    long X1 = floor(x / waveLength) * waveLength;
+    long X1 = static_cast<long>(floor(x / waveLength) * waveLength);
     long X2 = X1 + waveLength;
-    long Y1 = floor(y / waveLength) * waveLength;
+	long Y1 = static_cast<long>(floor(y / waveLength) * waveLength);
     long Y2 = Y1 + waveLength;
     
     //on calcule la valeur du bruit aux quatre points
@@ -72,6 +91,7 @@ double Noise::outputValue(long x, long y, int numberOfOctaves = 8) //l'octave 0 
     {
         outputValue += interpolatedNoise(x, y, octave);
     }
+	outputValue /= (2 * BASE_AMPLITUDE); // So we are sure we have values between 0 and 1
     return outputValue;
 }
 
@@ -81,12 +101,12 @@ void Noise::generateOutputFile(long size, int numberOfOctaves)
     perlinOutputFile = fopen("perlinOutputFile.txt", "w+");
     if (perlinOutputFile != NULL)
     {
-		for (double x = 0; x < size; x++)
+		for (long x = 0; x < size; x++)
         {
-			for (double y = 0; y < size; y++)
+			for (long y = 0; y < size; y++)
             {
                 double z = outputValue(x,y,numberOfOctaves);
-                fprintf(perlinOutputFile, "%f;%f;%f\n", x,y,z);
+                fprintf(perlinOutputFile, "%ld;%ld;%f\n", x,y,z);
             }
         }
         
--- bluemars/perlin.h
+++ bluemars/perlin.h
@@ -5,9 +5,10 @@
 
 #define BASE_WAVE_LENGTH 128
 #define BASE_AMPLITUDE 256
-#define MEAN_NOISE_VALUE 0
 #define PERSISTENCE 0.5 //coeff par lequel on multiplie l'amplitude à chaque octave (à chaque fois que la frequence double)
 #define MAX_SEED 1317624576693539401 // This is long_max / 7
+
+long murmurHash2(long seed);
 
 class Noise
 {
@@ -19,7 +20,7 @@ public:
 protected:
     long seed;
 	std::mt19937 rng; // Random number generator
-    double discreteNoise(long x, long y, long amplitude);
+    double discreteNoise(long x, long y, int octave);
     double interpolatedNoise(long x, long y, long waveLength = BASE_WAVE_LENGTH);
     double cosineInterpolate(long X1, long X2, double Z1, double Z2, long x);
     
