82d5650c8e0d27aede914e38bc5210a4a6cd7ef6
Vianney Tran <viq.dev@gmail.com>
2014-11-09 21:20:30 +0100

    Scrollable implementation of layer view

--- 2dvisualizer/layerVisualizer.cpp
+++ 2dvisualizer/layerVisualizer.cpp
@@ -45,9 +45,10 @@ void writeTGA2File(std::string filename, std::vector<unsigned char> tga_data)
 	file.close();
 }
 
-LayerWindow::LayerWindow(std::shared_ptr<BaseLayer>, std::string name, std::shared_ptr<sf::RenderWindow> p)
+LayerWindow::LayerWindow(std::shared_ptr<BaseLayer> l, std::string name, std::shared_ptr<sf::RenderWindow> p)
 {
 	layerName = name;
+	layer = l;
 	parent = p;
 	isScrolling = false;
 	startingXY = { 0, 0 };
@@ -62,6 +63,8 @@ LayerWindow::LayerWindow(std::shared_ptr<BaseLayer>, std::string name, std::shar
 	mainCanvas->GetSignal(sfg::Widget::OnMouseLeftRelease).Connect(std::bind(&LayerWindow::onStopScroll, this));
 	mainCanvas->GetSignal(sfg::Widget::OnMouseLeave).Connect(std::bind(&LayerWindow::onStopScroll, this));
 	mainCanvas->GetSignal(sfg::Widget::OnMouseMove).Connect(std::bind(&LayerWindow::onScroll, this));
+	mainWindow->GetSignal(sfg::Widget::OnSizeAllocate).Connect(std::bind(&LayerWindow::onResize, this));
+	update();
 }
 
 void LayerWindow::onStartScroll()
@@ -85,11 +88,96 @@ void LayerWindow::onScroll()
 		auto canvasPos = mainCanvas->GetAbsolutePosition();
 		auto movedPos = (mousePos - canvasPos) - startingXY;
 		currentUpperLeft = startingUpperLeft - static_cast<sf::Vector2<long>>(movedPos);
+		update();
 	}
 	//std::cout << "Moving" << std::endl;
+}
+void LayerWindow::onResize()
+{
+	update();
+}
+void LayerWindow::update()
+{
+	auto alloc = mainCanvas->GetAllocation();
+	currentLowerRight.x = currentUpperLeft.x + (long)round(alloc.width);
+	currentLowerRight.y = currentUpperLeft.y + (long)round(alloc.height);
+	std::pair<int, int> upperLeftChunk = std::make_pair<int, int>(floor((double)currentUpperLeft.x / layer->chunkSize), floor((double)currentUpperLeft.y / layer->chunkSize));
+	std::pair<int, int> lowerRightChunk = std::make_pair<int, int>(floor((double)currentLowerRight.x / layer->chunkSize), floor((double)currentLowerRight.y / layer->chunkSize));
+	
+	// 1 - Unload unused chunk (Too far)
+	{
+		auto it = chunk_sprites.begin();
+		while (it != chunk_sprites.end())
+		{
+			if (((*it)->chunk_x < upperLeftChunk.first - 2) || ((*it)->chunk_x > lowerRightChunk.first + 2) || ((*it)->chunk_y < upperLeftChunk.second - 2) || ((*it)->chunk_y > lowerRightChunk.first + 2))
+			{
+				// Chunk is outside of visible box + 2, remove it
+				std::cout << "Unloading chunk " << (*it)->chunk_x << ";" << (*it)->chunk_y << std::endl;
+				it = chunk_sprites.erase(it);
+			}
+			else
+			{
+				it++;
+			}
+		}
+	}
+	
+	// For each chunk in the visible box
+	for (int i = upperLeftChunk.first; i <= lowerRightChunk.first; i++)
+	{
+		for (int j = upperLeftChunk.second; j <= lowerRightChunk.second; j++)
+		{
+			layer->get(i * layer->chunkSize, j * layer->chunkSize); // For each chunk, be sure they are generated
+			
+			// 2 - Load needed chunk
+			bool toLoad = true;
+			for (auto it = chunk_sprites.begin(); it != chunk_sprites.end(); it++) // Check if the sprite is already loaded
+			{
+				if ((*it)->chunk_x == i && (*it)->chunk_y == j)
+				{
+					toLoad = false;
+					break;
+				}
+			}
+			if (toLoad) // Load Chunk
+			{
+				auto ptr = std::make_shared<ChunkSprite>(layer, i, j);
+				chunk_sprites.push_back(ptr);
+				std::cout << "Loading chunk " << i << ";" << j << std::endl;
+			}
+			
+		}
+	}
+	
+	mainCanvas->Bind();
+	mainCanvas->Clear(sf::Color(255, 0, 0, 0), true);
+
+	// 3 - Update sprite position & Draw
+	for (auto it = chunk_sprites.begin(); it != chunk_sprites.end(); it++)
+	{
+		long posX = (*it)->chunk_x * layer->chunkSize - currentUpperLeft.x;
+		long posY = (*it)->chunk_y * layer->chunkSize - currentUpperLeft.y;
+		(*it)->sprite.setPosition(sf::Vector2f(posX, posY));
+		mainCanvas->Draw((*it)->sprite);
+	}
+	mainCanvas->Display();
+	mainCanvas->Unbind();
+	
 }
 
 sfg::Window::Ptr LayerWindow::getWindow()
 {
 	return mainWindow;
+}
+
+
+// ChunkSprite
+ChunkSprite::ChunkSprite(std::shared_ptr<BaseLayer> layer, int cx, int cy)
+{
+	chunk_x = cx;
+	chunk_y = cy;
+	auto chunk = layer->getChunk(chunk_x, chunk_y);
+	auto tga = chunk2TGA(chunk, layer->chunkSize, layer->chunkSize);
+	texture.loadFromMemory(tga.data(), tga.size());
+	sprite.setTexture(texture);
 }
--- 2dvisualizer/layerVisualizer.h
+++ 2dvisualizer/layerVisualizer.h
@@ -10,6 +10,16 @@
 std::vector<unsigned char> chunk2TGA(std::shared_ptr<ChunkData> chunk, short width, short height); // Convert a chunk to a TGA Image
 void writeTGA2File(std::string filename, std::vector<unsigned char> tga_data); // For debug purpose, write a TGA to a file
 
+class ChunkSprite
+{
+public:
+	ChunkSprite(std::shared_ptr<BaseLayer> layer, int chunk_x, int chunk_y);
+	int chunk_x;
+	int chunk_y;
+	sf::Texture texture;
+	sf::Sprite sprite;
+};
+
 class LayerWindow // A scrollable widget linked to a layer
 {
 public:
@@ -17,10 +27,12 @@ public:
 	void onStartScroll();
 	void onStopScroll();
 	void onScroll();
-	void computeVisibleChunk(); // Compute and load which chunk are currently visible in the window
+	void onResize();
+	void update(); // Compute and load which chunk are currently visible in the window
 	sfg::Window::Ptr getWindow();
 protected:
 	std::string layerName;
+	std::shared_ptr<BaseLayer> layer;
 	sfg::Window::Ptr mainWindow;
 	sfg::Canvas::Ptr mainCanvas;
 	std::shared_ptr<sf::RenderWindow> parent;
@@ -28,7 +40,8 @@ protected:
 	sf::Vector2<float> startingXY; // Where the user started scrolling
 	sf::Vector2<long> startingUpperLeft;
 	sf::Vector2<long> currentUpperLeft; // The current inWorld coordinate of the upper left corner
-
+	sf::Vector2<long> currentLowerRight;
+	std::vector<std::shared_ptr<ChunkSprite>> chunk_sprites;
 };
 
 #endif
