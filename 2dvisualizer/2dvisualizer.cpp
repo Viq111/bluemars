c05ef64c743fa2e88915dc0c89b5c21847f43e47
Vianney Tran <viq.dev@gmail.com>
2014-11-09 18:32:41 +0100

    Working example of simpleAdditionLayer display

--- 2dvisualizer/2dvisualizer.cpp
+++ 2dvisualizer/2dvisualizer.cpp
@@ -2,53 +2,33 @@
 #include <SFML/Graphics.hpp>
 #include "2dvisualizer.h"
 
-const int SCREEN_WIDTH = 800;
-const int SCREEN_HEIGHT = 600;
-
-class HelloWorld {
-	public:
-		// Our button click handler.
-		void OnButtonClick();
-
-		void Run();
-
-	private:
-		// Create an SFGUI. This is required before doing anything with SFGUI.
-		sfg::SFGUI m_sfgui;
-
-		// Create the label pointer here to reach it from OnButtonClick().
-		sfg::Label::Ptr m_label;
-};
-
-void HelloWorld::OnButtonClick() {
-	m_label->SetText( "Hello SFGUI, pleased to meet you!" );
-}
-
-void HelloWorld::Run() {
+void MainWindow::main() {
 	// Create SFML's window.
-	sf::RenderWindow render_window( sf::VideoMode( SCREEN_WIDTH, SCREEN_HEIGHT ), "Hello world!" );
+	sf::RenderWindow render_window( sf::VideoMode( SCREEN_WIDTH, SCREEN_HEIGHT ), "BlueMars 2D Visualizer" );
 
-	// Create the label.
-	m_label = sfg::Label::Create( "Hello world!" );
-
-	// Create a simple button and connect the click signal.
-	auto button = sfg::Button::Create( "Greet SFGUI!" );
-	button->GetSignal( sfg::Widget::OnLeftClick ).Connect( std::bind( &HelloWorld::OnButtonClick, this ) );
+	// Create a simpleAdditionLayer
+	simpleAdditionLayer sa_layer;
+	sa_layer.get(0, 0); // Populate chunk 0,0
+	// Create the image
+	sf::Image layerImage;
+	auto tga = chunk2TGA(sa_layer.getChunk(0, 0),sa_layer.chunkSize, sa_layer.chunkSize);
+	layerImage.loadFromMemory(tga.data(), tga.size());
+	// Create a simpleAdditionLayer window
+	auto sfgImage = sfg::Image::Create();
+	sfgImage->SetImage(layerImage);
 
 	// Create a vertical box layouter with 5 pixels spacing and add the label
-	// and button to it.
 	auto box = sfg::Box::Create( sfg::Box::Orientation::VERTICAL, 5.0f );
-	box->Pack( m_label );
-	box->Pack( button, false );
+	box->Pack(sfgImage);
 
 	// Create a window and add the box layouter to it. Also set the window's title.
 	auto window = sfg::Window::Create();
-	window->SetTitle( "Hello world!" );
-	window->Add( box );
+	window->SetTitle("simpleAdditionLayer");
+	window->Add(box);
 
 	// Create a desktop and add the window to it.
 	sfg::Desktop desktop;
-	desktop.Add( window );
+	desktop.Add(window);
 
 	// We're not using SFML to render anything in this program, so reset OpenGL
 	// states. Otherwise we wouldn't see anything.
@@ -71,17 +51,15 @@ void HelloWorld::Run() {
 
 		// Update SFGUI with elapsed seconds since last call.
 		desktop.Update( clock.restart().asSeconds() );
-
 		// Rendering.
 		render_window.clear();
-		m_sfgui.Display( render_window );
+		root.Display(render_window);
 		render_window.display();
 	}
 }
 
 int main() {
-	HelloWorld hello_world;
-	hello_world.Run();
-
+	MainWindow window;
+	window.main();
 	return 0;
 }
--- 2dvisualizer/2dvisualizer.h
+++ 2dvisualizer/2dvisualizer.h
@@ -1,7 +1,18 @@
 #ifndef INCLUDE_2DVISUALIZER
 #define INCLUDE_2DVISUALIZER
-
 #include "layerVisualizer.h"
-
 #include "simpleAdditionLayer.h"
+
+// Default window
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+
+class MainWindow {
+public:
+	void main();
+private:
+	sfg::SFGUI root; // Create an SFGUI. This is required before doing anything with SFGUI.
+};
+
+
 #endif
