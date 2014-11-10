457d0a40bc72f9da06774af34e046010d2f1df05
Vianney Tran <viq.dev@gmail.com>
2014-11-10 15:33:27 +0100

    Removed unused portions of code

--- 2dvisualizer/2dvisualizer.cpp
+++ 2dvisualizer/2dvisualizer.cpp
@@ -7,34 +7,12 @@ void MainWindow::main() {
 	auto render_window = std::make_shared<sf::RenderWindow>(sf::VideoMode( SCREEN_WIDTH, SCREEN_HEIGHT ), "BlueMars 2D Visualizer" );
 	render_window->setVerticalSyncEnabled(true); // Enable vsync to stop hogging CPU
 
-	/*
-	// Create a simpleAdditionLayer
-	simpleAdditionLayer sa_layer;
-	sa_layer.get(0, 0); // Populate chunk 0,0
-	// Create the image
-	sf::Image layerImage;
-	auto tga = chunk2TGA(sa_layer.getChunk(0, 0),sa_layer.chunkSize, sa_layer.chunkSize);
-	layerImage.loadFromMemory(tga.data(), tga.size());
-	// Create a simpleAdditionLayer window
-	auto sfgImage = sfg::Image::Create();
-	sfgImage->SetImage(layerImage);
-
-	// Create a vertical box layouter with 5 pixels spacing and add the label
-	auto box = sfg::Box::Create( sfg::Box::Orientation::VERTICAL, 5.0f );
-	box->Pack(sfgImage);
-
-	// Create a window and add the box layouter to it. Also set the window's title.
-	auto window = sfg::Window::Create();
-	window->SetTitle("simpleAdditionLayer");
-	window->Add(box);
-	*/
 	// Create a window for simpleAdditionLayer
 	std::shared_ptr<simpleAdditionLayer> sa_layer = std::make_shared<simpleAdditionLayer>(simpleAdditionLayer());
 	LayerWindow sa_window(sa_layer, "simpleAdditionLayer", render_window);
 
 	// Create a desktop and add the window to it.
 	sfg::Desktop desktop;
-	//desktop.Add(window);
 	desktop.Add(sa_window.getWindow());
 
 	// We're not using SFML to render anything in this program, so reset OpenGL
@@ -45,19 +23,20 @@ void MainWindow::main() {
 	sf::Event event;
 	sf::Clock clock;
 
-	while( render_window->isOpen() ) {
+	while (render_window->isOpen())
+	{
 		// Event processing.
-		while( render_window->pollEvent( event ) ) {
-			desktop.HandleEvent( event );
-
+		while (render_window->pollEvent(event))
+		{
+			desktop.HandleEvent(event);
 			// If window is about to be closed, leave program.
-			if( event.type == sf::Event::Closed ) {
+			if (event.type == sf::Event::Closed)
+			{
 				render_window->close();
 			}
 		}
-
 		// Update SFGUI with elapsed seconds since last call.
-		desktop.Update( clock.restart().asSeconds() );
+		desktop.Update(clock.restart().asSeconds());
 		// Rendering.
 		render_window->clear();
 		root.Display(*render_window);
