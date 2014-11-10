9a3aff61763d2a3218fd6c7aba32e860f4c179d9
Romain Fihue <romain.fihue@student.ecp.fr>
2014-11-10 13:27:52 +0100

    Reverting Y&X

--- bluemars/elevation.cpp
+++ bluemars/elevation.cpp
@@ -33,13 +33,16 @@ std::vector<float> elevation::generateData(long seed, long x_offset, long y_offs
 	int size = chunkSize/NB_THREADS;
 	std::vector<std::future<std::vector<float>>> values;
 	long x_min;long x_max; long y_min; long y_max;
+	std::cout << "x_offset: "<< x_offset<< " y_offset: "<< y_offset << std::endl;
 	for(int i=0; i<NB_THREADS; i++){
 		x_min = x_offset + i*size; // =x_offset when i=0
 		x_max = x_min + size; //=x_offset+chunk_size when i=0
-		values.push_back(std::async(std::launch::async, &elevation::generatePartData, this, seed, x_min, x_max, chunkSize));
+		std::cout << "\tx_min: "<< x_min<< " x_max: "<< x_max << std::endl;
+		values.push_back(std::async(std::launch::async, &elevation::generatePartData, this, seed, x_min, x_max, y_offset, chunkSize));
 	}
 	std::vector<float> result;
 	result.reserve(chunkSize*chunkSize);
+	//for(auto i = values.size()-1; i < values.size(); i--){
 	for(std::vector<int>::size_type i = 0; i != values.size(); i++){
 		std::vector<float> pdata = values[i].get();
 		result.insert(result.begin(),pdata.begin(),pdata.end());
@@ -49,13 +52,13 @@ std::vector<float> elevation::generateData(long seed, long x_offset, long y_offs
 }
 
 std::vector<float> elevation::generatePartData(long seed, long x_min, long x_max,
-																							 int size){
+																							 long y_offset, int size){
 	Noise noiseInstance = Noise(seed);
 	std::vector<float> result;
 	result.reserve((x_max-x_min)*size);
-	for (int i_x = x_min; i_x < x_max; ++i_x)
+	for (int i_y = y_offset; i_y < size+y_offset; ++i_y)
 	{
-		for (int i_y = 0; i_y < size; ++i_y)
+		for (int i_x = x_min; i_x < x_max; ++i_x)
 		{
 			result.push_back((float) noiseInstance.outputValue(i_x,i_y));
 		}
